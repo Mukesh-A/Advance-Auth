cfc89d1ca674b66d1ed92d37e5fea7d40586330a
Mukesh-a <mukeshabhi08@gmail.com>
2022-10-23 21:11:18 +0530

    Auth

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/user-routes");
+const cookieParser = require("cookie-parser"); 
+
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
 mongoose
@@ -9,7 +12,7 @@ mongoose
     "mongodb+srv://admin:kR4SGspYjVnaeHmy@cluster0.13vfmv2.mongodb.net/auth?retryWrites=true&w=majority"
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(4000);
     console.log("database is connected listenig to localhost ");
   })
   .catch((err) => console.log(err));
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,9 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { json } = require("express");
+const JWT_SECRET_KEY = "MyKey";
+
 const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -39,12 +43,62 @@ const login = async (req, res, next) => {
   } catch (err) {
     return new Error(err);
   }
-  const isPaswordCorrect = bcrypt.compareSync(password, existingUser.password);
-  if (existingUser && isPaswordCorrect) {
-    res.send("login success");
-  } else {
+
+  if (!existingUser) {
     return res.status(400).json({ message: "user not found" });
   }
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "invalid email/password" });
+  }
+  const token = jwt.sign({ id: existingUser.id }, JWT_SECRET_KEY, {
+    expiresIn: "1hr",
+  });
+  res.cookie(String(existingUser._id), token, {
+    path: "/",
+    expires: new Date(Date.now() + 1000 * 30),
+    httpOnly: true,
+    sameSite: "lax",
+  });
+
+  return res
+    .status(200)
+    .json({ message: "successfully logged in", user: existingUser, token });
 };
+
+//verify the user with token
+const verifyToken = (req, res, next) => {
+  const header = req.headers["authorization"];
+  const token = header.split(" ")[1];
+  if (!token) {
+    res.status(404).json({ message: "no token found" });
+  }
+  jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      res.status(400).json({ message: "invalid token" });
+    }
+    console.log(user.id);
+    req.id = user.id;
+  });
+  next();
+};
+
+//getting user
+const getUser = async (req, res, next) => {
+  const userId = req.id;
+  let user;
+  try {
+    user = await User.findById(userId, "-password");
+  } catch (err) {
+    return new Error(err);
+  }
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+  return res.status(200).json({ user });
+};
+
 exports.signup = signup;
 exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
